fb32f3de38d076681cbe687edae9438118f60e65
reebuker <spontofs@gmail.com>
2026-05-23 14:09:50 +0300

    Сделал цикл обучения модели, с сохранением истории потерь. Пока без теста

--- lung_cancer/main.py
+++ lung_cancer/main.py
@@ -1,7 +1,34 @@
+import torch
+
+
 from dataset import get_data_loaders
 from model import create_model
+from trainer import Trainer
 
-get_data_loaders()
-create_model()
+def main():
+    train_loader, val_loader, test_loader = get_data_loaders()
+    model = create_model()
 
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Выбрано вычислительное устройство:", device)
 
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    trainer = Trainer(
+        model=model,
+        device=device,
+        optimizer=optimizer,
+        criterion=criterion
+    )
+
+    print("Старт обучения...")
+    history = trainer.fit(
+        train_loader=train_loader,
+        val_loader=val_loader,
+        epochs=10
+    )
+    print("Обучение завершено!")
+
+if __name__ == "__main__":
+    main()
--- lung_cancer/trainer.py
+++ lung_cancer/trainer.py
@@ -1,11 +1,59 @@
 import torch
 
 class Trainer:
-    def __init__(self, model, optimizer, criterion):
-        self.model = model
+    def __init__(self, model, device, optimizer, criterion):
+        self.model = model.to(device)
+        self.device = device
         self.optimizer = optimizer
         self.criterion = criterion
         self.history = {"train_loss": [], "val_loss": []}
 
-    # def train_epoch(self, dataloader):
+    def train_epoch(self, dataloader):
+        """Одна эпоха обучения."""
+        self.model.train()
+        running_loss = 0.0
+        for images, labels in dataloader:
+            images = images.to(self.device)
+            labels = labels.to(self.device)
+
+            self.optimizer.zero_grad()
+            outputs = self.model(images)
+            loss = self.criterion(outputs, labels)
+            loss.backward()
+            self.optimizer.step()
+
+            running_loss += loss.item() * images.size(0)
+        return running_loss / len(dataloader.dataset)
+
+    def val_epoch(self, dataloader):
+        """Одна эпоха валидации."""
+        self.model.eval()
+        running_loss = 0.0
+
+        # Отключаем вычисление градиентов
+        with torch.no_grad():
+            for images, labels in dataloader:
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+                
+                outputs = self.model(images)
+                loss = self.criterion(outputs, labels)
+
+                running_loss += loss.item() * images.size(0)
+        return running_loss / len(dataloader.dataset)
+
+
+    def fit(self, train_loader, val_loader, epochs):
+        """ Основной цикл обучения """
+        for epoch in range(epochs):
+            train_loss = self.train_epoch(train_loader)
+            val_loss = self.val_epoch(val_loader)
+            
+            self.history["train_loss"].append(train_loss)
+            self.history["val_loss"].append(val_loss)
+
+            print(f"Epoch {epoch+1}/{epochs} | "
+                  f"Train Loss: {train_loss:.4f} | "
+                  f"Val Loss: {val_loss:.4f}")
+        return self.history
     
